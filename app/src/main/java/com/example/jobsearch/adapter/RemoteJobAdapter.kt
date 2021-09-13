41ab9edbfaa43915e57050939822198ce9115299
ImVikasJi <vbhadoriya3@gmail.com>
2021-09-13 19:10:47 +0530

    Added RemoteJobAdapter.kt

---
+++ app/src/main/java/com/example/jobsearch/adapter/RemoteJobAdapter.kt
@@ -0,0 +1,65 @@
+package com.example.jobsearch.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.navigation.findNavController
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.jobsearch.databinding.JobLayoutAdapterBinding
+import com.example.jobsearch.models.Job
+import com.example.jobsearch.ui.fragments.MainFragmentDirections
+import com.example.jobsearch.viewmodel.RemoteJobViewModel
+import kotlinx.coroutines.internal.MissingMainCoroutineDispatcherFactory
+
+class RemoteJobAdapter : RecyclerView.Adapter<RemoteJobAdapter.RemoteJobViewHolder>() {
+
+    private var _binding: JobLayoutAdapterBinding? = null
+    private var binding = _binding!!
+
+    inner class RemoteJobViewHolder(itemBinding: JobLayoutAdapterBinding) :
+        RecyclerView.ViewHolder(itemBinding.root)
+
+    private val differCallback = object : DiffUtil.ItemCallback<Job>() {
+        override fun areItemsTheSame(oldItem: Job, newItem: Job): Boolean {
+            return oldItem.url == newItem.url
+        }
+
+        override fun areContentsTheSame(oldItem: Job, newItem: Job): Boolean {
+            return oldItem == newItem
+        }
+    }
+
+    val differ = AsyncListDiffer(this, differCallback)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RemoteJobViewHolder {
+        binding =
+            JobLayoutAdapterBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return RemoteJobViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: RemoteJobViewHolder, position: Int) {
+        val currentJob = differ.currentList[position]
+
+        holder.itemView.apply {
+            Glide.with(this)
+                .load(currentJob.companyLogoUrl)
+                .into(binding.ivCompanyLogo)
+
+            binding.tvCompanyName.text = currentJob.companyName
+            binding.tvJobLocation.text = currentJob.candidateRequiredLocation
+            binding.tvJobTitle.text = currentJob.title
+            binding.tvJobType.text = currentJob.jobType
+
+            val dateJob = currentJob.publicationDate.split("T")
+            binding.tvDate.text = dateJob.get(0)
+        }.setOnClickListener { mView ->
+            val direction = MainFragmentDirections.actionMainFragmentToJobDetailsFragment()
+
+            mView.findNavController().navigate(direction)
+        }
+    }
+
+    override fun getItemCount(): Int = differ.currentList.size
+}
