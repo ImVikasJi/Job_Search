5f030f96d976cd93df26835dca11295fdb60f896
ImVikasJi <vbhadoriya3@gmail.com>
2021-09-15 09:32:27 +0530

    Added try{}catch{} in RemoteJobViewModel.kt

--- app/src/main/java/com/example/jobsearch/viewmodel/RemoteJobViewModel.kt
+++ app/src/main/java/com/example/jobsearch/viewmodel/RemoteJobViewModel.kt
@@ -4,22 +4,30 @@ import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.viewModelScope
+import com.bumptech.glide.load.engine.Resource
 import com.example.jobsearch.models.RemoteJobResponse
 import com.example.jobsearch.repository.RemoteJobRepository
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import okio.IOException
 
 class RemoteJobViewModel(
     app: Application,
     private val remoteJobRepository: RemoteJobRepository
 ) : AndroidViewModel(app) {
 
-    init {
-        viewModelScope.launch(Dispatchers.IO) {
-            remoteJobRepository.getRemoteJobResponse()
-        }
-    }
 
     val jobs: LiveData<RemoteJobResponse>
         get() = remoteJobRepository.jobs
+
+    init {
+        viewModelScope.launch(Dispatchers.IO) {
+            try {
+                remoteJobRepository.getRemoteJobResponse()
+            } catch (t: Throwable) {
+
+            }
+        }
+    }
+
 }
